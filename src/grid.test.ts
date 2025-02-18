6f5f2c1ff10481e19b9db0045040a20877b22da4
André Mathlener <info@macmannes.nl>
2025-02-18 22:06:14 +0100

    Added test which forces us to implement Grid.nextGeneration()

--- src/grid.test.ts
+++ src/grid.test.ts
@@ -5,7 +5,7 @@ import { Grid } from './grid';
 import { CellState } from './cell-state';
 
 describe('Grid', () => {
-    it('should evolve correctly using the Game of Life rule', () => {
+    it('should evolve correctly with a 2x2 grid using the Game of Life rule', () => {
         const grid = new Grid([
             new Cell(new Position(0, 0), CellState.alive()),
             new Cell(new Position(0, 1), CellState.alive()),
@@ -21,6 +21,45 @@ describe('Grid', () => {
                 new Cell(new Position(0, 1), CellState.alive()),
                 new Cell(new Position(1, 0), CellState.alive()),
                 new Cell(new Position(1, 1), CellState.alive()),
+            ]),
+        );
+    });
+
+    it('should evolve correctly with a 4x4 grid, specifying only the alive cells', () => {
+        // .XX.
+        // X..X
+        // .X..
+        // ..X.
+
+        const grid = new Grid([
+            new Cell(new Position(0, 1), CellState.alive()),
+            new Cell(new Position(0, 2), CellState.alive()),
+            new Cell(new Position(1, 0), CellState.alive()),
+            new Cell(new Position(1, 3), CellState.alive()),
+            new Cell(new Position(2, 1), CellState.alive()),
+            new Cell(new Position(3, 2), CellState.alive()),
+        ]);
+
+        const nextGeneration = grid.nextGeneration();
+
+        // .X..
+        // XX..
+        // .XX.
+        // ..X.
+        expect(nextGeneration).toStrictEqual(
+            new Grid([
+                new Cell(new Position(0, 0), CellState.alive()),
+                new Cell(new Position(0, 1), CellState.alive()),
+                new Cell(new Position(0, 2), CellState.alive()),
+                new Cell(new Position(0, 3), CellState.alive()),
+
+                new Cell(new Position(1, 0), CellState.alive()),
+                new Cell(new Position(1, 1), CellState.alive()),
+
+                new Cell(new Position(2, 1), CellState.alive()),
+                new Cell(new Position(2, 2), CellState.alive()),
+
+                new Cell(new Position(3, 2), CellState.alive()),
             ]),
         );
     });
